28ad4ef4fb21d923596eacfd8821a770dfc27405
WilliamMBaker <willbee8888@gmail.com>
2025-02-23 21:42:14 +0000

    Add WMO weather code defines

---
+++ eink-idf-port/main/WMOWeatherCodes.h
@@ -0,0 +1,116 @@
+
+// No percipitation
+#define WMO_NO_PERCIP_CODE_00	00 // Cloud development not observed or not observable, Characteristic change of the state of sky during the past hour
+#define WMO_NO_PERCIP_CODE_01	01 // Clouds generally dissolving or becoming less developed, Characteristic change of the state of sky during the past hour
+#define WMO_NO_PERCIP_CODE_02	02 // State of sky on the whole unchanged, Characteristic change of the state of sky during the past hour
+#define WMO_NO_PERCIP_CODE_03	03 // Clouds generally forming or developing, Characteristic change of the state of sky during the past hour
+#define WMO_NO_PERCIP_CODE_04	04 // Visibility reduced by smoke, e.g. veldt or forest fires, industrial smoke or volcanic ashes
+#define WMO_NO_PERCIP_CODE_05	05 // Haze
+#define WMO_NO_PERCIP_CODE_06	06 // Widespread dust in suspension in the air, not raised by wind at or near the station at the time of observation
+#define WMO_NO_PERCIP_CODE_07	07 // Dust or sand raised by wind at or near the station at the time of observation, but no well developed dust whirl(s) or sand whirl(s), and no duststorm or sandstorm seen
+#define WMO_NO_PERCIP_CODE_08	08 // Well developed dust whirl(s) or sand whirl(s) seen at or near the station during the preceding hour or at the time ot observation, but no duststorm or sandstorm
+#define WMO_NO_PERCIP_CODE_09	09 // Duststorm or sandstorm within sight at the time of observation, or at the station during the preceding hour
+#define WMO_NO_PERCIP_CODE_10	10 // Mist
+#define WMO_NO_PERCIP_CODE_11	11 // Patches, shallow fog or ice fog at the station, whether on land or sea, not deeper than about 2 metres on land or 10 metres at sea
+#define WMO_NO_PERCIP_CODE_12	12 // More or less continuous, shallow fog or ice fog at the station, whether on land or sea, not deeper than about 2 metres on land or 10 metres at sea
+#define WMO_NO_PERCIP_CODE_13	13 // Lightning visible, no thunder heard
+#define WMO_NO_PERCIP_CODE_14	14 // Precipitation within sight, not reaching the ground or the surface of the sea
+#define WMO_NO_PERCIP_CODE_15	15 // Precipitation within sight, reaching the ground or the surface of the sea, but distant, i.e. estimated to be more than 5 km from the station
+#define WMO_NO_PERCIP_CODE_16	16 // Precipitation within sight, reaching the ground or the surface of the sea, near to, but not at the station
+#define WMO_NO_PERCIP_CODE_17	17 // Thunderstorm, but no precipitation at the time of observation
+#define WMO_NO_PERCIP_CODE_18	18 // Squalls 	at or within sight of the station during the preceding hour or at the time of observation
+#define WMO_NO_PERCIP_CODE_19	19 // Funnel cloud (Tornado cloud or water-spout)
+
+// Percipitation
+#define WMO_PERCIP_CODE_20	20 // Drizzle (not freezing) or snow grains, not falling as shower(s)
+#define WMO_PERCIP_CODE_21	21 // Rain (not freezing), not falling as shower(s)
+#define WMO_PERCIP_CODE_22	22 // Snow, not falling as shower(s)
+#define WMO_PERCIP_CODE_23	23 // Rain and snow or ice pellets, not falling as shower(s)
+#define WMO_PERCIP_CODE_24	24 // Freezing drizzle or freezing rain, not falling as shower(s)
+#define WMO_PERCIP_CODE_25	25 // Shower(s) of rain
+#define WMO_PERCIP_CODE_26	26 // Shower(s) of snow, or of rain and snow
+#define WMO_PERCIP_CODE_27	27 // Shower(s) of hail*, or of rain and hail*
+#define WMO_PERCIP_CODE_28	28 // Fog or ice fog
+#define WMO_PERCIP_CODE_29	29 // Thunderstorm (with or without precipitation)
+
+// Blown particles (Duststorm, sandstorm, drifting or blowing snow)
+#define WMO_PART_CODE_30	30 // Slight or moderate duststorm or sandstorm, has decreased during the preceding hour
+#define WMO_PART_CODE_31	31 // Slight or moderate duststorm or sandstorm, no appreciable change during the preceding hour
+#define WMO_PART_CODE_32	32 // Slight or moderate duststorm or sandstorm, has begun or has increased during the preceding hour
+#define WMO_PART_CODE_33	33 // Severe duststorm or sandstorm, has decreased during the preceding hour
+#define WMO_PART_CODE_34	34 // Severe duststorm or sandstorm, no appreciable change during the preceding hour
+#define WMO_PART_CODE_35	35 // Severe duststorm or sandstorm, has begun or has increased during the preceding hour
+#define WMO_PART_CODE_36	36 // Slight or moderate blowing snow generally low (below eye level)
+#define WMO_PART_CODE_37	37 // Heavy drifting snow generally low (below eye level)
+#define WMO_PART_CODE_38	38 // Slight or moderate blowing snow generally high (above eye level)
+#define WMO_PART_CODE_39	39 // Heavy drifting snow generally high (above eye level)
+
+// Fog or ice fog at the time of observation
+#define WMO_ICE_FOG_CODE_40	40 // Fog or ice fog at a distance at the time of observation, but not at the station during the preceding hour, the fog or ice fog extending to a level above that of the observer
+#define WMO_ICE_FOG_CODE_41	41 // Fog or ice fog in patches
+#define WMO_ICE_FOG_CODE_42	42 // Fog or ice fog, sky visible, has become thinner during the preceding hour
+#define WMO_ICE_FOG_CODE_43	43 // Fog or ice fog, sky invisible, has become thinner during the preceding hour
+#define WMO_ICE_FOG_CODE_44	44 // Fog or ice fog, sky visible, no appreciable change during the preceding hour
+#define WMO_ICE_FOG_CODE_45	45 // Fog or ice fog, sky invisible, no appreciable change during the preceding hour
+#define WMO_ICE_FOG_CODE_46	46 // Fog or ice fog, sky visible, has begun or has become thicker during the preceding hour
+#define WMO_ICE_FOG_CODE_47	47 // Fog or ice fog, sky invisible, has begun or has become thicker during the preceding hour
+#define WMO_ICE_FOG_CODE_48	48 // Fog, depositing rime, sky visible
+#define WMO_ICE_FOG_CODE_49	49 // Fog, depositing rime, sky invisible
+
+// Drizzle
+#define WMO_DRIZZLE_CODE_50	50 // Drizzle, not freezing, intermittent, slight at time of observation
+#define WMO_DRIZZLE_CODE_51	51 // Drizzle, not freezing, continuous, slight at time of observation
+#define WMO_DRIZZLE_CODE_52	52 // Drizzle, not freezing, intermittent, moderate at time of observation
+#define WMO_DRIZZLE_CODE_53	53 // Drizzle, not freezing, continuous, moderate at time of observation
+#define WMO_DRIZZLE_CODE_54	54 // Drizzle, not freezing, intermittent, heavy (dense) at time of observation
+#define WMO_DRIZZLE_CODE_55	55 // Drizzle, not freezing, continuous, heavy (dense) at time of observation
+#define WMO_DRIZZLE_CODE_56	56 // Drizzle, freezing, slight
+#define WMO_DRIZZLE_CODE_57	57 // Drizzle, freezing, moderate or heavy (dence)
+#define WMO_DRIZZLE_CODE_58	58 // Drizzle and rain, slight
+#define WMO_DRIZZLE_CODE_59	59 // Drizzle and rain, moderate or heavy
+
+// Rain
+#define WMO_RAIN_CODE_60	60 // Rain, not freezing, intermittent, slight at time of observation
+#define WMO_RAIN_CODE_61	61 // Rain, not freezing, continuous, slight at time of observation
+#define WMO_RAIN_CODE_62	62 // Rain, not freezing, intermittent, moderate at time of observation
+#define WMO_RAIN_CODE_63	63 // Rain, not freezing, continuous, moderate at time of observation
+#define WMO_RAIN_CODE_64	64 // Rain, not freezing, intermittent, heavy at time of observation
+#define WMO_RAIN_CODE_65	65 // Rain, not freezing, continuous, heavy at time of observation
+#define WMO_RAIN_CODE_66	66 // Rain, freezing, slight
+#define WMO_RAIN_CODE_67	67 // Rain, freezing, moderate or heavy (dence)
+#define WMO_RAIN_CODE_68	68 // Rain or drizzle and snow, slight
+#define WMO_RAIN_CODE_69	69 // Rain or drizzle and snow, moderate or heavy
+
+// Solid precipitation not in showers
+#define WMO_SOLID_PERCIP_CODE_70	70 // Intermittent fall of snowflakes, slight at time of observation
+#define WMO_SOLID_PERCIP_CODE_71	71 // Continuous fall of snowflakes, slight at time of observation
+#define WMO_SOLID_PERCIP_CODE_72	72 // Intermittent fall of snowflakes, moderate at time of observation
+#define WMO_SOLID_PERCIP_CODE_73	73 // Continuous fall of snowflakes, moderate at time of observation
+#define WMO_SOLID_PERCIP_CODE_74	74 // Intermittent fall of snowflakes, heavy at time of observation
+#define WMO_SOLID_PERCIP_CODE_75	75 // Continuous fall of snowflakes, heavy at time of observation
+#define WMO_SOLID_PERCIP_CODE_76	76 // Diamond dust (with or without fog)
+#define WMO_SOLID_PERCIP_CODE_77	77 // Snow grains (with or without fog)
+#define WMO_SOLID_PERCIP_CODE_78	78 // Isolated star-like snow crystals (with or without fog)
+#define WMO_SOLID_PERCIP_CODE_79	79 // Ice pellets
+
+// Showery precipitation, or precipitation with current or recent thunderstorm
+#define WMO_SHOWER_CODE_80	80 // Rain shower(s), slight
+#define WMO_SHOWER_CODE_81	81 // Rain shower(s), moderate or heavy
+#define WMO_SHOWER_CODE_82	82 // Rain shower(s), violent
+#define WMO_SHOWER_CODE_83	83 // Shower(s) of rain and snow mixed, slight
+#define WMO_SHOWER_CODE_84	84 // Shower(s) of rain and snow mixed, moderate or heavy
+#define WMO_SHOWER_CODE_85	85 // Snow shower(s), slight
+#define WMO_SHOWER_CODE_86	86 // Snow shower(s), moderate or heavy
+#define WMO_SHOWER_CODE_87	87 // Shower(s) of snow pellets or small hail, with or without rain or rain and snow mixed, slight
+#define WMO_SHOWER_CODE_88	88 // Shower(s) of snow pellets or small hail, with or without rain or rain and snow mixed, moderate or heavy
+#define WMO_SHOWER_CODE_89	89 // Shower(s) of hail*, with or without rain or rain and snow mixed, not associated with thunder, slight
+#define WMO_SHOWER_CODE_90	90 // Shower(s) of hail*, with or without rain or rain and snow mixed, not associated with thunder, moderate or heavy
+#define WMO_SHOWER_CODE_91	91 // Slight rain at time of observation, Thunderstorm during the preceding hour but not at time of observation
+#define WMO_SHOWER_CODE_92	92 // Moderate or heavy rain at time of observation, Thunderstorm during the preceding hour but not at time of observation
+#define WMO_SHOWER_CODE_93	93 // Slight snow, or rain and snow mixed or hail** at time of observation, Thunderstorm during the preceding hour but not at time of observation
+#define WMO_SHOWER_CODE_94	94 // Moderate or heavy snow, or rain and snow mixed or hail** at time of observation, Thunderstorm during the preceding hour but not at time of observation
+#define WMO_SHOWER_CODE_95	95 // Thunderstorm, slight or moderate, without hail** but with rain and/or snow at time of observation, Thunderstorm at time of observation
+#define WMO_SHOWER_CODE_96	96 // Thunderstorm, slight or moderate, with hail** at time of observation, Thunderstorm at time of observation
+#define WMO_SHOWER_CODE_97	97 // Thunderstorm, heavy, without hail** but with rain and/or snow at time of observation, Thunderstorm at time of observation
+#define WMO_SHOWER_CODE_98	98 // Thunderstorm combined with duststorm or sandstorm at time of observation, Thunderstorm at time of observation
+#define WMO_SHOWER_CODE_99	99 // Thunderstorm, heavy, with hail** at time of observation, Thunderstorm at time of observation
